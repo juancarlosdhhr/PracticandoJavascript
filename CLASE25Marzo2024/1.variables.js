4518cd9192234c4b55f082309fe94144b829b621
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-05-07 17:53:01 +0100

    1

--- CLASE25Marzo2024/1.variables.js
+++ CLASE25Marzo2024/1.variables.js
@@ -1,3 +1,17 @@
 let FirstName = "Hola Mundo";
 
-console.log (FirstName);
+console.log (FirstName); 
+
+
+/* NODE es el programa para que Javascript pueda ejecutar en nuestra parte 
+(la parte del servidor, es decir en la partre del ordenador), porque el ordeandor no sabe Javascript,
+el que sabe Javascript es el navegador.
+ */
+
+/* NOTA:  Para que pille bien la ruta lo mejor es copiarla directamente desde el explorador de Windows
+La que me ha funcionado es esta: 
+
+PS C:\Users\Usuario\Documents\GitHub\PracticandoJavascript> cd \Users\Usuario\Documents\GitHub\PracticandoJavascript
+\CLASE25Marzo2024  
+
+PS C:\Users\Usuario\Documents\GitHub\PracticandoJavascript\CLASE25Marzo2024> */
