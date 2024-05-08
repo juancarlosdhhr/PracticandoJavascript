06f346380e3878c7902fcbb8322649e796cef7a0
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-05-08 02:06:49 +0100

    4

---
+++ 3.tiposdedatos.js
@@ -0,0 +1,14 @@
+
+
+let frase = 'Como dijo un sabio: "A programar se aprende programando" ' ;
+
+let edad = 35;
+
+let booleano = false; /* Es una traduccion al castellano de lo que significaba antes 0 o 1 */
+
+let list = ["pimiento", "seitan", "tomate", "macarrones"] /* Esto es un ARRAY , es decir un listado de cosas
+ *//*  Los arrays se empiezan a contabilizar desde 0,1,2,3...Es decir la primera posicion es el 0 */
+/* Si lo pongo entre comillas es una frase o String
+
+Si pongo un numero sin comillas entonces es un numero y se puede operar con el. */
+
